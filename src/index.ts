4a43464a358e0dddd89dd04e21514abe8d8d650a
Stepan <kurakste@gmail.com>
2019-07-21 20:55:19 +0300

    fix formating

--- src/index.ts
+++ src/index.ts
@@ -7,17 +7,17 @@ if (cluster.isMaster) {
   const cpusCount = os.cpus().length;
   console.log(`CPUs: ${cpusCount}`);
   console.log(`Master started Pid: ${pid}`)
-  for (let i=0; i<cpusCount-1; i++) {
+  for (let i = 0; i < cpusCount - 1; i++) {
     const worker = cluster.fork();
     worker.on('exit', ariseWorker);
   }
-} 
+}
 
-function ariseWorker(this:any) {
-    console.log(`Worker died! PID: ${this.process.pid}`);
-    const nworker =cluster.fork();
-    console.log(`new worker started: ${nworker.process.pid}`);
-    nworker.on('exit', ariseWorker);
+function ariseWorker(this: any) {
+  console.log(`Worker died! PID: ${this.process.pid}`);
+  const nworker = cluster.fork();
+  console.log(`new worker started: ${nworker.process.pid}`);
+  nworker.on('exit', ariseWorker);
 }
 
 if (cluster.isWorker) {
--- src/worker.ts
+++ src/worker.ts
@@ -5,12 +5,12 @@ const port = 3030;
 app.get('/', payloader);
 app.listen(port, () => console.log(`Example app listening on port ${port}! PID: ${process.pid}`));
 
-function payloader(req:Express.Request, res:any) {
+function payloader(req: Express.Request, res: any) {
   let iter = 3e8;
-  while (iter>=0) {
+  while (iter >= 0) {
     let a = 12;
     let b = Math.sin(a);
-    let c =Math.log(a);
+    let c = Math.log(a);
     iter--;
   }
   res.send('Done');
